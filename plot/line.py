816fb2eeda9b4564a5b4c9fe6fa04d9583d9cd8d
lipo <lipo8081@gmail.com>
2020-07-06 01:38:27 +0800

    添加测试的line模块

---
+++ plot/line.py
@@ -0,0 +1,13 @@
+from lib import BasePlotable
+
+import matplotlib.pyplot as plt
+
+
+class Line(BasePlotable):
+
+    def __init__(self, label: str = None):
+        self.label = label
+
+    def plot(self, data):
+        plt.plot(data, label=self.label)
+    
