74c9cd53a47275913570f5af2c840c524ad52d9b
lipo <lipo8081@gmail.com>
2020-07-06 01:39:57 +0800

    添加特征提取函数

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ from invoke import tasks
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from data import XLSXReader
+from data import XLSXReader, ExtractData
 from plot import Line
 from lib import TimeGroup
 
@@ -19,7 +19,10 @@ def agg_data(c):
     file_list = filter(lambda fname: fname.startswith("2011"), os.listdir("./asset"))
     data = None
     for fname in file_list:
-        _d = XLSXReader(f"./asset/{fname}", usecols=["交易时间", "WEIGHT_FACT_CASH"], index_col=0)
+        _d = XLSXReader(
+            f"./asset/{fname}", 
+            usecols=["交易时间", "WEIGHT_FACT_CASH"], 
+            index_col=0).convert_index_to_datetime()
         if not data:
             data = _d
         else:
@@ -31,3 +34,14 @@ def agg_data(c):
 def feature_extract(c):
     """特征提取
     """
+    assert os.path.exists("asset/agg.xlsx"), "请先运行 inv agg-data 进行数据聚合"
+    data = XLSXReader(f"asset/agg.xlsx", index_col=0).convert_index_to_datetime()
+    # data.convert_index_to_datetime()
+    data_5_min = data.pipe(TimeGroup("5Min")).pipe(ExtractData())
+    data_10_min = data.pipe(TimeGroup("10Min")).pipe(ExtractData())
+    data_15_min = data.pipe(TimeGroup("15Min")).pipe(ExtractData())
+
+    # save data
+    data_5_min.save("asset/5min.xlsx")
+    data_10_min.save("asset/10min.xlsx")
+    data_15_min.save("asset/15min.xlsx")
