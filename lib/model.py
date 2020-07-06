2ddbbb0774aefd839ab1e6f3e96f22fc887607cf
lipo <lipo8081@gmail.com>
2020-07-07 01:10:22 +0800

    结构化model流程

--- lib/model.py
+++ lib/model.py
@@ -3,5 +3,15 @@ from lib.metas import MetaModel
 
 class BaseModel(metaclass=MetaModel):
 
+    def setup(self):
+        """初始化模型
+        """
+        pass
+
+    def fit(self):
+        """训练模型
+        """
+        raise NotImplementedError
+
     def predict(self, input_data):
         raise NotImplementedError
