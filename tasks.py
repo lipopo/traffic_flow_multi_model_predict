e040d6f35827e2b9ee9081bef1ad1d58f7867606
lipo <lipo8081@gmail.com>
2020-07-08 06:12:09 +0800

    修改tasks.py模块代码风格

--- tasks.py
+++ tasks.py
@@ -1,13 +1,9 @@
 # 任务模块
-# 打包主要的命令行指令
 import os
 
 from invoke import tasks
-import pandas as pd
-import matplotlib.pyplot as plt
 
 from data import XLSXReader, ExtractData
-from plot import Line
 from lib import TimeGroup, BaseModel, MetaModel
 
 
@@ -15,13 +11,14 @@ from lib import TimeGroup, BaseModel, MetaModel
 def agg_data(c):
     """合并已经有的数据
     """
-    count_data = lambda d: d.count()
-    file_list = filter(lambda fname: fname.startswith("2011"), os.listdir("./asset"))
+    file_list = filter(
+            lambda fname: fname.startswith("2011"),
+            os.listdir("./asset"))
     data = None
     for fname in file_list:
         _d = XLSXReader(
-            f"./asset/{fname}", 
-            usecols=["交易时间", "WEIGHT_FACT_CASH"], 
+            f"./asset/{fname}",
+            usecols=["交易时间", "WEIGHT_FACT_CASH"],
             index_col=0).convert_index_to_datetime()
         if not data:
             data = _d
@@ -35,7 +32,10 @@ def feature_extract(c):
     """特征提取
     """
     assert os.path.exists("asset/agg.xlsx"), "请先运行 inv agg-data 进行数据聚合"
-    data = XLSXReader(f"asset/agg.xlsx", index_col=0).convert_index_to_datetime()
+    data = XLSXReader(
+            "asset/agg.xlsx",
+            index_col=0).convert_index_to_datetime()
+
     # data.convert_index_to_datetime()
     data_5_min = data.pipe(TimeGroup("5Min")).pipe(ExtractData())
     data_10_min = data.pipe(TimeGroup("10Min")).pipe(ExtractData())
@@ -46,6 +46,7 @@ def feature_extract(c):
     data_10_min.save("asset/10min.xlsx")
     data_15_min.save("asset/15min.xlsx")
 
+
 @tasks.task
 def list_models(self):
     """查询可用的模型列表
@@ -54,10 +55,16 @@ def list_models(self):
     print("当前可用的预测模型有: ")
     for model in models.__dict__.values():
         if type(model) == MetaModel and issubclass(model, BaseModel):
-                print(f"    {model.name}-{model.__doc__}")
+            print(f"    {model.name}-{model.__doc__}")
 
 
 @tasks.task
 def run_model_strategy(self, model_name):
     """运行策略，对于输入数据进行预测
+    """
+
+
+@tasks.task()
+def fit_model(self, model_name, train_data, test_data, save=False):
+    """训练模型
     """
