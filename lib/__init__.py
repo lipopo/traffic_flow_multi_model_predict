e8e20689a8d060864c35924b79671b33afbcd80c
lipo <lipo8081@gmail.com>
2020-07-10 20:16:21 +0800

    变变更lib库的引用

--- lib/__init__.py
+++ lib/__init__.py
@@ -3,5 +3,12 @@ from lib.model import BaseModel
 from lib.loss import BaseLoss
 from lib.ga import GA, Individual, Population
 from lib.pipe import Pipe
-from lib.feature_extractor import FeatureExtractor, TimeGroup
+from lib.feature_extractor import FeatureExtractor, \
+        TimeGroup, TimeSplit
 from lib.plot import BasePlotable
+
+
+__all__ = (
+        MetaPlotable, MetaModel, BaseModel, Individual,
+        BaseLoss, GA, Population, Pipe, FeatureExtractor,
+        TimeGroup, TimeSplit, BasePlotable)
