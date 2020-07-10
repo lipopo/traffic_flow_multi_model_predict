e91742f6a2686b9803df4589c7264dc8f3660eff
lipo <lipo8081@gmail.com>
2020-07-10 20:13:53 +0800

    update the export module in data package

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,5 @@
 # 数据包
 # 所有数据处理相关的对象 都会收录在此
 from data.xlsx_reader import XLSXReader
-from data.extract_feature import ExtractData
+from data.preprocess_feature import PreprocessData
+from data.extract_feautre import ExtractData
