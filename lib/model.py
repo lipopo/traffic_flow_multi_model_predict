fb725335cc0f3fc84cc9e994b93bde0e22b81df7
lipo <lipo8081@gmail.com>
2020-07-10 20:18:27 +0800

    更新model模块中部分架构

--- lib/model.py
+++ lib/model.py
@@ -1,7 +1,26 @@
+from collections import namedtuple
+
 from lib.metas import MetaModel
 
 
+DataSet = namedtuple('DataSet', ['data', 'target'])
+
+
 class BaseModel(metaclass=MetaModel):
+
+    losses = []
+    test_dataset = None  # 测试数据集
+
+    def snap(self):
+        for loss in self.losses:
+            loss.snap_point()
+
+    def clear_loss(self):
+        for loss in self.losses:
+            loss.clear()
+
+    def set_test_dataset(self, test_dataset):
+        self.test_dataset = DataSet(**test_dataset)
 
     def set_parameter(self, parameter):
         """由个体操作，用于设置参数
