59df777968ba8770c372d1e39c7a47eb629aaf3a
lipo <lipo8081@gmail.com>
2020-07-10 20:40:09 +0800

    修复引入名称错误bug

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,5 @@
 # 数据包
 # 所有数据处理相关的对象 都会收录在此
 from data.xlsx_reader import XLSXReader
-from data.preprocess_feature import PreprocessData
-from data.extract_feautre import ExtractData
+from data.preprocess_data import PreprocessData
+from data.extract_feature import ExtractData
