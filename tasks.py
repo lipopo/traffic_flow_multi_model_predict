d1bcdab21ac0e890b53da672e81bb6c753880045
lipo <lipo8081@gmail.com>
2020-07-12 16:56:43 +0800

    配置训练流程

--- tasks.py
+++ tasks.py
@@ -1,4 +1,3 @@
-# 任务模块
 import os
 import warnings
 
@@ -8,8 +7,10 @@ warnings.simplefilter('ignore')
 
 from invoke import tasks
 
-from data import XLSXReader, ExtractData
-from lib import TimeGroup, TimeSplit, BaseModel, MetaModel
+from config import time_range, model_config
+from data import XLSXReader, ExtractData, PreprocessData
+from lib import TimeGroup, TimeSplit, BaseModel, MetaModel, DataPipe
+from plot import Line
 from model import BP
 
 
@@ -43,16 +44,29 @@ def feature_extract(c):
             index_col=0).convert_index_to_datetime()
 
     # data.convert_index_to_datetime()
-    data_30_sec = data.pipe(TimeGroup("30s")).pipe(ExtractData())
-    data_5_min = data.pipe(TimeGroup("5Min")).pipe(ExtractData())
-    data_10_min = data.pipe(TimeGroup("10Min")).pipe(ExtractData())
-    data_15_min = data.pipe(TimeGroup("15Min")).pipe(ExtractData())
+    data_30_sec = data.pipe(TimeGroup("30s")).pipe(PreprocessData())
+    data_5_min = data.pipe(TimeGroup("5Min")).pipe(PreprocessData())
+    data_10_min = data.pipe(TimeGroup("10Min")).pipe(PreprocessData())
+    data_15_min = data.pipe(TimeGroup("15Min")).pipe(PreprocessData())
 
     # save data
     data_30_sec.save("asset/30sec.xlsx")
     data_5_min.save("asset/5min.xlsx")
     data_10_min.save("asset/10min.xlsx")
     data_15_min.save("asset/15min.xlsx")
+
+
+def get_model(model_name):
+    models = __import__("model")
+    for model in models.__dict__.values():
+        if type(model) == MetaModel and issubclass(model, BaseModel) and model.name == model_name:
+            return model
+    return None
+
+
+def get_parameter(model_name, _time):
+    Model = get_model(model_name)
+    return Model.load_parameter(_time)
 
 
 @tasks.task
@@ -68,22 +82,54 @@ def list_models(self):
 
 @tasks.task
 def run_model_strategy(self, model_name):
-    """运行策略，对于输入数据进行预测
+    """运行策略
+    """
+    data = XLSXReader(
+            "./asset/30sec.xlsx", 
+            index_col=0
+        ).convert_index_to_datetime()
+    Model = get_model(model_name)
+    model = Model(**model_config.get(model_name))
+    for idx, _time in enumerate(time_range):
+        model.set_parameter(get_parameter(model_name, _time))
+        predict = model.predict
+        predict_data = data.pipe(TimeGroup(_time)).pipe(ExtractData()).pipe(lambda data: data[:, :-1]).pipe(predict)
+
+        # plot data
+        predict_data.pipe(lambda d: d['target']).pipe(Line(label=f"{_time}_predict", index=len(time_range) * 100 + 11 + idx))
+        data.pipe(TimeGroup(_time)).pipe(ExtractData()).pipe(lambda data: data[:, -1]).pipe(Line(label=f"{_time}_true"))
+        Line.finish()
+    Line.show()
+
+
+@tasks.task()
+def fit_model(self, model_name):
+    """训练模型
     """
     # load data first
     data_base = XLSXReader(
             "asset/30sec.xlsx",
             index_col=0).convert_index_to_datetime()
     # split data
-    data = data_base.pipe(TimeSplit('2011-05-26 00:00:00')).data
+    train_data, test_data = tuple(data_base.pipe(TimeSplit('2011-05-26 00:00:00')).data)
+    train_data_pipe = DataPipe(train_data)  # 训练数据Pipe
+    test_data_pipe = DataPipe(test_data)  # 测试数据Pipe
 
-    # 构建模型
-    # model = BP(layer_sizes=(5, 10, 1))
-    # model = BP(layer_sizes=(5, 10, 1))
-    # print(model, model.parameter)
+    data = {}
+    for _time in time_range:
+        data[f"train_{_time}"] = train_data_pipe.pipe(TimeGroup(_time)).pipe(ExtractData())
+        data[f"test_{_time}"] = test_data_pipe.pipe(TimeGroup(_time)).pipe(ExtractData())
 
+    Model = get_model(model_name)
 
-@tasks.task()
-def fit_model(self, model_name, train_data, test_data, save=False):
-    """训练模型
-    """
+    if Model is None:
+        c.echo(f"{model_name}模型不存在")
+        return
+
+    # 构建模型 - 按照不同的时间范围，以及不同的模型构建
+    # 训练模型
+    for _time in time_range:
+        model = Model(**model_config.get(model_name))
+        data.get(f"train_{_time}").pipe(lambda d: model.fit(d[:, :-1], d[:, -1]))
+        model.save_parameter(_time)
+
