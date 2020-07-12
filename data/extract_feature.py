4b92d5e8de2926b72e78fea89b18c9a0eef5d378
lipo <lipo8081@gmail.com>
2020-07-12 16:55:37 +0800

    配置特征提取流程

--- data/extract_feature.py
+++ data/extract_feature.py
@@ -1,14 +1,24 @@
+import pandas as pd
+
 from lib.feature_extractor import FeatureExtractor
 
 
 class ExtractData(FeatureExtractor):
     """数据特征提取
     """
-    def extract(self, data):
-        
-        # 统计数据量
-        count_data = data.count()
+    def extract(self, data: pd.DataFrame):
+        mean_data = data.mean().iloc[:, 0]  # 均值数据
+        var_data = data.var().iloc[:, 0]  # 方差
+        diff_data = mean_data.diff()  # 一阶导数
+        diff_diff_data = mean_data.diff().diff()  # 二阶导数
+        target_data = mean_data.shift(-1)  # 目标值
 
-        # 统计其他参数
-        mae_data = data.mae()
-        
+        # 构建特征表
+        feature_data = pd.DataFrame(
+            data={
+                "mean": mean_data, "var": var_data,
+                "diff": diff_data, "diff2": diff_diff_data,
+                "target": target_data
+            }
+        )
+        return feature_data.dropna().to_numpy()
