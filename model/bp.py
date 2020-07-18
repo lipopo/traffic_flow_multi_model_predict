4973f83b7ee836bcc54730756ec69dc18ae9aca6
lipo <lipo8081@gmail.com>
2020-07-19 05:58:43 +0800

    添加参数描述 #88

--- model/bp.py
+++ model/bp.py
@@ -115,10 +115,26 @@ class BP(BaseModel):
 
 
 class ParameterIndividual(Individual):
+    @staticmethod
+    def rand_feature(parameter_size, loss_function):
+        parameters = np.random.random(parameter_size)
+        return {
+            "parameters": parameters,
+            "loss_func": loss_function
+        }
+
+    @property
+    def loss_function(self):
+        return self.feature.get("loss_func")
+
+    @property
+    def parameters(self):
+        return self.feature.get("parameters")
+
     def calc_fitness(self):
         """计算适应度
         """
-        loss_values = self.loss_function(self.prameters)
+        loss_values = self.loss_function(self.parameters)
         # indivdual handler their own fitness and the model
         # handler the loss calc method and others
         return -loss_values[0]
@@ -143,14 +159,6 @@ class ParameterIndividual(Individual):
             if random.random() > mutation_value:
                 parameters[p_idx] = random.random()
         self.feature["parameters"] = parameters
-
-    @staticmethod
-    def rand_feature(parameter_size, loss_function):
-        parameters = np.random.random(parameter_size)
-        return {
-            "parameters": parameters,
-            "loss_func": loss_function
-        }
 
 
 class GaBP(BP):
