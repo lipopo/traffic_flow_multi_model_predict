eb5d93116c43f4a1c1039b13d6a5615426fbb9d8
lipo <lipo8081@gmail.com>
2020-07-22 00:28:58 +0800

    部分模型预构建

--- model/ga_knn_lssvm.py
+++ model/ga_knn_lssvm.py
@@ -1,13 +1,52 @@
-from lib import BaseModel, GA, Individual, Population
+from typing import Dict, List
+
+import numpy as np
+
+from lib import BaseModel, GA, Population, ParameterIndividual
+from loss import MaeLoss
 
 
 class GaKnnLssvm(BaseModel):
     name = "GAKNNLSSVM"
     __doc__ = "基于遗传算法优化的knn_lssvm联合预测模型"
+    losses = [MaeLoss()]
+    use_ga = True
+    _ga = None
 
-    class ParameterIndividual(Individual):
-        @staticmethod
-        def rand_feature():
-            pass
+    def __init__(self, train_kwargs, ga_parameter, parameter_scaler):
+        self.train_kwargs = train_kwargs
 
-    ga = GA(Population.generate_population(ParameterIndividual, 100))
+    def predict(self, input_data: np.array) -> Dict[str, np.array]:
+        pass
+
+    def fit(self, input_data: np.array, target_data: np.array):
+        pass
+
+    def set_parameter(self, parameter):
+        pass
+
+    def loss(self, parameter) -> List[float]:
+        self.set_parameter(parameter)
+        preb_value = self.predict(self.input_data)
+        true_value = self.target_data
+        _loss_list = []
+        for loss in self.losses:
+            _loss_value = loss(preb_value, true_value)
+            _loss_list.append(_loss_value)
+        return _loss_list
+
+    @property
+    def parameter(self) -> np.array:
+        pass
+
+    @property
+    def ga(self):
+        if self._ga is None:
+            self._ga = GA(
+                Population.generate_population(
+                    ParameterIndividual,
+                    100,
+                    parameter_size=len(
+                        self.parameter),
+                    loss_function=self.loss))
+        return self._ga
--- model/lssvr.py
+++ model/lssvr.py
@@ -1,18 +1,43 @@
-from lib import BaseModel, GA, Individual, Population
+from lib import BaseModel, GA, Population, ParameterIndividual
 
 
 class Lssvr(BaseModel):
     name = "LSSVR"
     __doc__ = "LSSVR预测模型"
+    _model = None
+
+    def __init__(self, train_kwargs):
+        self.train_kwargs = train_kwargs
+
+    @property
+    def model(self):
+        if self._model is None:
+            self._model = None
+        return self._model
+
+    def predict(self, input_data):
+        target_data = self.model.predict(input_data)
+        return {
+            "target_data": target_data,
+            "input_data": input_data
+        }
+
+    def fit(self, input_data, target_data):
+        self.model.fit(input_data, target_data)
 
 
 class GaLssvr(Lssvr):
     name = "GALSSVR"
     __doc__ = "基于遗传算法优化的LSSVR预测模型"
 
-    class ParameterIndividual(Individual):
-        @staticmethod
-        def rand_feature():
-            pass
-
-    ga = GA(Population.generate_population(ParameterIndividual, 100))
+    @property
+    def ga(self):
+        if self._ga is None:
+            self._ga = GA(
+                Population.generate_population(
+                    ParameterIndividual,
+                    100,
+                    loss_function=self.loss,
+                    parameter_size=len(
+                        self.parameter)))
+        return self._ga
