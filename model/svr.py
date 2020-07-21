c101fd579af3752ac6717a7fbeb352498d1c7f17
lipo <lipo8081@gmail.com>
2020-07-22 00:28:30 +0800

    完成svr模型

--- model/svr.py
+++ model/svr.py
@@ -2,7 +2,7 @@ from sklearn.svm import SVR
 import numpy as np
 
 from loss import MaeLoss
-from lib import BaseModel, GA, Individual, Population
+from lib import BaseModel, GA, Individual, Population, ParameterIndividual
 
 
 class Svr(BaseModel):
@@ -11,31 +11,14 @@ class Svr(BaseModel):
     _model = None
     losses = [MaeLoss()]
 
-    def __init__(self, train_kwargs, parameter_list):
+    def __init__(self, train_kwargs):
         self.train_kwargs = train_kwargs
-        self.parameter_list = parameter_list
 
     @property
     def model(self):
         if self._model is None:
             self._model = SVR(**self.train_kwargs)
         return self._model
-
-    @property
-    def parameter(self):
-        param_dict = self.model.get_params()
-        param_list = []
-        for param_name in self.parameter_list:
-            param_list.append(param_dict.get(param_name, None))
-        return np.array(param_list)
-
-    def set_parameter(self, parameter):
-        """设置指定的参数组合
-        """
-        parameter_dict = {}
-        for idx, name in enumerate(self.paramter_name):
-            parameter_dict[name] = parameter[idx]
-        self.model.set_params(**parameter_dict)
 
     def predict(self, input_data):
         """模型预测结果
@@ -51,55 +34,18 @@ class Svr(BaseModel):
         """
         self.model.fit(input_data, target_data)
 
-    def loss(self, parameter):
-        """计算指参数下的残差
-        """
-        self.set_parameter(parameter)
-        preb_value = self.predict(self.input_data).get("target")
-        true_value = self.target_data
-        _loss_list = []
-        for _loss in self.losses:
-            _loss_value = _loss.calc_loss(preb_value, true_value)
-            _loss_list.append(_loss_value)
-        return _loss_list
-
-
-class ParameterIndividual(Individual):
-
-    @staticmethod
-    def rand_feature(parameter_size, loss_function):
-        parameters = np.random.random(parameter_size)
-        return {
-            "parameters": parameters,
-            "loss_func": loss_function
-        }
-
-    @property
-    def loss_funcion(self):
-        return self.feature.get("loss_funcion")
-
-    @property
-    def parameters(self):
-        return self.feature.get("parameters")
-
-    def calc_fitness(self):
-        """计算适应度"""
-        pass
-
-    def corssover(self, other):
-        """交叉"""
-        pass
-
-    def mutation(self, mutation_value):
-        """变异"""
-        pass
-
 
 class GaSvr(Svr):
     name = "GASVR"
     __doc__ = "基于遗传算法优化的SVR预测模型"
     use_ga = True
     _ga = None
+
+    def __init__(self, *args, **kwargs):
+        self.parameter_list = kwargs.pop("parameter_list")
+        self.parameter_scaler = kwargs.pop("parameter_scaler")
+        self.ga_parameter = kwargs.pop("ga_parameter")
+        super().__init__(*args, **kwargs)
 
     @property
     def ga(self):
@@ -111,3 +57,37 @@ class GaSvr(Svr):
                     loss_function=self.loss
                 ))
         return self._ga
+
+    @property
+    def parameter(self):
+        param_dict = self.model.get_params()
+        param_list = []
+        for param_name in self.parameter_list:
+            param_list.append(param_dict.get(param_name, None))
+        return np.array(param_list)
+
+    def set_parameter(self, parameter):
+        """设置指定的参数组合
+        """
+        parameter_dict = {}
+        parameter_scaler = self.parameter_scaler
+        for idx, name in enumerate(self.parameter_list):
+            min_val, max_val = tuple(parameter_scaler[idx])
+            val_range = max_val - min_val
+            parameter_dict[name] = parameter[idx] * \
+                val_range + min_val
+        self.model.set_params(**parameter_dict)
+
+    def loss(self, parameter):
+        """计算指参数下的残差
+        """
+        self.set_parameter(parameter)
+        # fit first
+        self.model.fit(self.input_data, self.target_data)
+        preb_value = self.predict(self.input_data).get("target_data")
+        true_value = self.target_data
+        _loss_list = []
+        for _loss in self.losses:
+            _loss_value = _loss.calc_loss(preb_value, true_value)
+            _loss_list.append(_loss_value)
+        return _loss_list
