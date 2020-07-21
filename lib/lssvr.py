10ba6959b0a7962d5ff23d3942064da86003c8fa
lipo <lipo8081@gmail.com>
2020-07-22 07:36:13 +0800

    初始化lssvr框架

---
+++ lib/lssvr.py
@@ -0,0 +1,23 @@
+
+
+class Lssvm:
+    pass
+
+
+class Lssvr:
+    _model = None
+
+    def __init__(self):
+        pass
+
+    def fit(self, input_data, target_data):
+        pass
+
+    def predict(self, input_data):
+        pass
+
+    def set_param(self, parameter_dict):
+        pass
+
+    def get_param(self):
+        pass
