42af4a08104c0435a80ea5707c1be38bb66b005e
lipo <lipo8081@gmail.com>
2020-07-21 19:29:02 +0800

    Add some change

--- model/svr.py
+++ model/svr.py
@@ -1,4 +1,5 @@
 from sklearn.svm import SVR
+import numpy as np
 
 from loss import MaeLoss
 from lib import BaseModel, GA, Individual, Population
@@ -10,8 +11,9 @@ class Svr(BaseModel):
     _model = None
     losses = [MaeLoss()]
 
-    def __init__(self, train_kwargs):
-       self.train_kwargs = train_kwargs
+    def __init__(self, train_kwargs, parameter_list):
+        self.train_kwargs = train_kwargs
+        self.parameter_list = parameter_list
 
     @property
     def model(self):
@@ -22,14 +24,18 @@ class Svr(BaseModel):
     @property
     def parameter(self):
         param_dict = self.model.get_params()
-        c = param_dict.get("C")
-        gamma = param_dict.get("param")
-        epsilon = param_dict.get("epsilon")
-        return [c, gamma, epsilon]
+        param_list = []
+        for param_name in self.parameter_list:
+            param_list.append(param_dict.get(param_name, None))
+        return np.array(param_list)
 
     def set_parameter(self, parameter):
-        c, gamma, epsilon = tuple(parameter)
-        self.model.set_params(C=c, gammma=gamma, epsilon=epsilon)
+        """设置指定的参数组合
+        """
+        parameter_dict = {}
+        for idx, name in enumerate(self.paramter_name):
+            parameter_dict[name] = parameter[idx]
+        self.model.set_params(**parameter_dict)
 
     def predict(self, input_data):
         """模型预测结果
@@ -45,9 +51,48 @@ class Svr(BaseModel):
         """
         self.model.fit(input_data, target_data)
 
+    def loss(self, parameter):
+        """计算指参数下的残差
+        """
+        self.set_parameter(parameter)
+        preb_value = self.predict(self.input_data).get("target")
+        true_value = self.target_data
+        _loss_list = []
+        for _loss in self.losses:
+            _loss_value = _loss.calc_loss(preb_value, true_value)
+            _loss_list.append(_loss_value)
+        return _loss_list
+
 
 class ParameterIndividual(Individual):
-    pass
+
+    @staticmethod
+    def rand_feature(parameter_size, loss_function):
+        parameters = np.random.random(parameter_size)
+        return {
+            "parameters": parameters,
+            "loss_func": loss_function
+        }
+
+    @property
+    def loss_funcion(self):
+        return self.feature.get("loss_funcion")
+
+    @property
+    def parameters(self):
+        return self.feature.get("parameters")
+
+    def calc_fitness(self):
+        """计算适应度"""
+        pass
+
+    def corssover(self, other):
+        """交叉"""
+        pass
+
+    def mutation(self, mutation_value):
+        """变异"""
+        pass
 
 
 class GaSvr(Svr):
@@ -61,5 +106,8 @@ class GaSvr(Svr):
         if self._ga is None:
             self._ga = GA(
                 Population.generate_population(
-                    ParameterIndividual, 1000))
+                    ParameterIndividual, 100,
+                    parameter_size=len(self.parameter),
+                    loss_function=self.loss
+                ))
         return self._ga
