3d9370cd0204c2a889c9a9adcccb0bd818dc19c7
lipo <lipo8081@gmail.com>
2020-07-29 22:58:49 +0800

    解决内存泄漏问题

--- lib/lssvr.py
+++ lib/lssvr.py
@@ -1,4 +1,7 @@
 """Least Squares Support Vector Regression."""
+import gc
+import tracemalloc
+
 import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.metrics.pairwise import rbf_kernel
@@ -6,7 +9,10 @@ from scipy.sparse import linalg
 
 
 class LSSVR(BaseEstimator, RegressorMixin):
-    def __init__(self, C=1, kernel='rbf', gamma=0.1, epsilon=0.1):
+    _rbf = None
+    idxs = None
+
+    def __init__(self, C=1, kernel='linear', gamma=0.1):
         """
         @parameter C float
         @description 松弛系数
@@ -21,9 +27,7 @@ class LSSVR(BaseEstimator, RegressorMixin):
         self.supportVectorLabels = None
         self.C = C
         self.gamma = gamma
-        self.epsilon = epsilon
         self.kernel = kernel
-        self.idxs = None
         self.K = None
         self.bias = None
         self.alphas = None
@@ -45,20 +49,22 @@ class LSSVR(BaseEstimator, RegressorMixin):
         @parameter y_train np.array
         @description 训练目标数据
         """
-        if isinstance(self.idxs, type(None)):
-            self.idxs = np.ones(x_train.shape[0], dtype=bool)
+        # tracemalloc.start()
+        # if isinstance(self.idxs, type(None)):
+        #     self.idxs = np.ones(x_train.shape[0], dtype=bool)
+        self.idxs = np.zeros(x_train.shape[0], dtype=bool)
+        self.idxs[:] = True
 
-        self.supportVectors = x_train[self.idxs, :]
-        self.supportVectorLabels = y_train[self.idxs]
+        self.supportVectors = x_train[self.idxs, :].astype(np.float32)
+        self.supportVectorLabels = y_train[self.idxs].astype(np.float32)
 
-        K = self.kernel_func(
+        self.K = self.kernel_func(
             self.kernel,
             x_train,
             self.supportVectors,
             self.gamma)
 
-        self.K = K
-        OMEGA = K
+        OMEGA = self.K
         OMEGA[self.idxs, np.arange(OMEGA.shape[1])] += 1 / self.C
 
         D = np.zeros(np.array(OMEGA.shape) + 1)
@@ -77,13 +83,16 @@ class LSSVR(BaseEstimator, RegressorMixin):
             z = linalg.lsmr(D.T, t)[0]
         except BaseException:
             z = np.linalg.pinv(D).T @ t.ravel()
-
-        # alloc
-        del D
-
         self.bias = z[0]
         self.alphas = z[1:]
         self.alphas = self.alphas[self.idxs]
+        del D, z
+        gc.collect()
+        # snapshot = tracemalloc.take_snapshot()
+        # state = snapshot.statistics('lineno')
+        # print("-"*10)
+        # for _s in state[:10]:
+        #     print(_s)
 
     def predict(self, x_test):
         K = self.kernel_func(
@@ -91,7 +100,6 @@ class LSSVR(BaseEstimator, RegressorMixin):
             x_test,
             self.supportVectors,
             self.gamma)
-
         return (K @ self.alphas) + self.bias
 
     def kernel_func(self, kernel, u, v, gamma):
@@ -115,3 +123,9 @@ class LSSVR(BaseEstimator, RegressorMixin):
         A = self.alphas.reshape(-1, 1) @ self.alphas.reshape(-1, 1).T
         W = A @ self.K[self.idxs, :]
         return np.sqrt(np.sum(np.diag(W)))
+
+    # @property
+    # def rbf(self):
+    #     if not self._rbf:
+    #         self._rbf = self.gamma * RBF()
+    #     return self._rbf
