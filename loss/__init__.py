94f662d57e17d3602cd51c5bf5bd8851860ad0b8
lipo <lipo8081@gmail.com>
2020-08-03 06:36:59 +0800

    添加ecloss引用

--- loss/__init__.py
+++ loss/__init__.py
@@ -1,5 +1,6 @@
 # loss包
 from loss.mae_loss import MaeLoss
 from loss.mse_loss import MseLoss
+from loss.ec_loss import EcLoss
 
-__all__ = ("MaeLoss", "MseLoss")
+__all__ = ("MaeLoss", "MseLoss", "EcLoss")
